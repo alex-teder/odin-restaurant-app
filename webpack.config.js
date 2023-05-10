20243593b8f57742f5fcd64db7d80fe715060b21
Teder-kedr <124440529+Teder-kedr@users.noreply.github.com>
2023-05-10 20:07:01 +0400

    added html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   // devtool: "none",
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "bundle-[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
     rules: [
       {
